ca7f4f7b500d319a842d92d947cbb14e20157423
EJMac <79925738+EJMac@users.noreply.github.com>
2022-01-29 13:22:39 -0500

    foam

--- Objects/walls/Foam/Foam.gd
+++ Objects/walls/Foam/Foam.gd
@@ -2,12 +2,13 @@ extends StaticBody2D
 
 export (PackedScene) var wave = preload("res://Objects/Wave.tscn")
 onready var point = global_position
+var description:="Blocks all types of signal"
+export var movable := true
+export var spinable := true
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_to_group("drag")
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("wavicle"):
-		print("a2d body entered")
 		body.queue_free()
