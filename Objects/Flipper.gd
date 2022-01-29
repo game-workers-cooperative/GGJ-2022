4fea7ce297e3c9bdbf6a7d798fa9d8745b37a370
Nemesis <shuklaakshatascom@gmail.com>
2022-01-29 21:23:18 +0530

    Added Flipper and renamed .gitignore

---
+++ Objects/Flipper.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var switched_wavicles: Array = []
+
+func _ready() -> void:
+	var err = self.connect("body_entered", self, "_on_wavicle_entered")
+	if err != OK: printerr(err)
+
+func _on_wavicle_entered(wavicle: Node):
+	if switched_wavicles.find(wavicle.get_instance_id()) == -1 and wavicle.is_in_group("wavicle"): wavicle.call_deferred("change_state")
+	switched_wavicles.append(wavicle.get_instance_id())
+
+func _on_wavicle_exited(wavicle: Node):
+	switched_wavicles.remove(switched_wavicles.find(wavicle.get_instance_id()))
--- Objects/wavicle.gd
+++ Objects/wavicle.gd
@@ -13,11 +13,47 @@ func _ready():
 	else:
 		$wave.set_visible(false)
 		$wave_shape.disabled = true
+
 var clock :=0.0
 func _physics_process(delta):
 	clock += delta
-	if is_wave:
-		scale = Vector2.ONE*(1+sin(4*clock)*0.25)
+#	if is_wave:
+#		scale = Vector2.ONE*(1+sin(4*clock)*0.25)
+
+func change_state() -> void:
+	is_wave = !is_wave
+	
+	$particle.set_visible(!is_wave)
+	$particle_shape.disabled = is_wave
+	$wave.set_visible(is_wave)
+	$wave_shape.disabled = !is_wave
+#	print(is_wave)
+#	print("Is Wave:", is_wave)
+#	print("Particle:", !is_wave)
+#	print("Particle Shape", !is_wave)
+#	print("Wave:", is_wave)
+#	print("Wave Shape:", is_wave)
+#	if is_wave:
+#		# Disable Particle Mode
+#		$particle.set_visible(false)
+#		$particle_shape.disabled = true
+#		# Enable Wave Mode
+#		$wave.set_visible(true)
+#		$wave_shape.disabled = false
+#	else:
+#		# Disable Particle Mode
+#		$particle.set_visible(true)
+#		$particle_shape.disabled = false
+#		# Enable Wave Mode
+#		$wave.set_visible(false)
+#		$wave_shape.disabled = true
+		
+		# Disable Wave Mode
+#		$wave.set_visible(false)
+#		$wave_shape.disabled = true
+		# Enable Particle Mode
+#		$particle.set_visible(true)
+#		$particle_shape.disabled = false
 
 func _screen_exited():
 	queue_free()
