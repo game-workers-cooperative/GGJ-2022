65d939b4f7ec5d312f64afc24768243643302d7a
Nemesis <shuklaakshatascom@gmail.com>
2022-01-29 21:28:49 +0530

    Fixed Signal and removed dead code comments

--- Objects/Flipper.gd
+++ Objects/Flipper.gd
@@ -5,6 +5,8 @@ var switched_wavicles: Array = []
 func _ready() -> void:
 	var err = self.connect("body_entered", self, "_on_wavicle_entered")
 	if err != OK: printerr(err)
+	err = self.connect("body_exited", self, "_on_wavicle_exited")
+	if err != OK: printerr(err)
 
 func _on_wavicle_entered(wavicle: Node):
 	if switched_wavicles.find(wavicle.get_instance_id()) == -1 and wavicle.is_in_group("wavicle"): wavicle.call_deferred("change_state")
--- Objects/wavicle.gd
+++ Objects/wavicle.gd
@@ -17,8 +17,8 @@ func _ready():
 var clock :=0.0
 func _physics_process(delta):
 	clock += delta
-#	if is_wave:
-#		scale = Vector2.ONE*(1+sin(4*clock)*0.25)
+	if is_wave:
+		scale = Vector2.ONE*(1+sin(4*clock)*0.25)
 
 func change_state() -> void:
 	is_wave = !is_wave
@@ -27,33 +27,6 @@ func change_state() -> void:
 	$particle_shape.disabled = is_wave
 	$wave.set_visible(is_wave)
 	$wave_shape.disabled = !is_wave
-#	print(is_wave)
-#	print("Is Wave:", is_wave)
-#	print("Particle:", !is_wave)
-#	print("Particle Shape", !is_wave)
-#	print("Wave:", is_wave)
-#	print("Wave Shape:", is_wave)
-#	if is_wave:
-#		# Disable Particle Mode
-#		$particle.set_visible(false)
-#		$particle_shape.disabled = true
-#		# Enable Wave Mode
-#		$wave.set_visible(true)
-#		$wave_shape.disabled = false
-#	else:
-#		# Disable Particle Mode
-#		$particle.set_visible(true)
-#		$particle_shape.disabled = false
-#		# Enable Wave Mode
-#		$wave.set_visible(false)
-#		$wave_shape.disabled = true
-		
-		# Disable Wave Mode
-#		$wave.set_visible(false)
-#		$wave_shape.disabled = true
-		# Enable Particle Mode
-#		$particle.set_visible(true)
-#		$particle_shape.disabled = false
 
 func _screen_exited():
 	queue_free()
